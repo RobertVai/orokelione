b4be89d519951b901e20b287912dc9e26a2b2e9e
RobertVai <vatechovskij@gmail.com>
2025-08-02 20:35:47 +0300

    header and hero components created

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,46 @@
+import styles from "@/components/Hero/Hero.module.css";
+
+export default function Hero() {
+  return (
+    <section className={styles.hero}>
+      <div className={styles.overlay}>
+        <div className={styles.container}>
+          <h4 className={styles.title}>Make Memories Worldwide.</h4>
+            <div className={styles.contentBox}>
+          <div className={styles.navTabs}>
+            <button className={styles.activeTab}>✈ Flights</button>
+            <button>🚗 Car rentals</button>
+            <button>🏨 Hotel</button>
+          </div>
+
+          <form className={styles.searchForm}>
+            <div className={styles.rowTop}>
+              <span>Round trip ▼</span>
+              <span>1 Traveller, Economy ⚙</span>
+              <label>
+                <input type="checkbox" /> Direct flights only
+              </label>
+            </div>
+
+            <div className={styles.rowBottom}>
+              <div className={styles.inputGroup}>
+                <label>From:</label>
+                <input type="text" placeholder="Vilnius (VNO)" />
+              </div>
+              <div className={styles.inputGroup}>
+                <label>To:</label>
+                <input type="text" placeholder="ex. Riga (RIX)" />
+              </div>
+              <div className={styles.inputGroup}>
+                <label>Date:</label>
+                <input type="text" placeholder="Departure - Arrival" />
+              </div>
+              <button className={styles.searchButton}>Search flight</button>
+            </div>
+          </form>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+}
