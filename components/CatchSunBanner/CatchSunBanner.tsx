eaba26a3363c557b509a6b184ce2b9ead30c572d
RobertVai <vaitechovskij@gmail.com>
2025-08-02 23:18:58 +0300

    home page layout with hero, buttons, promo banner, price tag (styles + structure)

---
+++ components/CatchSunBanner/CatchSunBanner.tsx
@@ -0,0 +1,22 @@
+import styles from './CatchSunBanner.module.css';
+
+const CatchSunBanner = () => {
+  return (
+    <section className={styles.banner}>
+      
+      <div className={styles.priceTag}>From €49.99</div>
+
+      
+      <div className={styles.content}>
+        <h2 className={styles.title}>Let’s catch the sun!</h2>
+        <button className={styles.button}>Book now</button>
+      </div>
+
+
+
+    </section>
+    
+  );
+};
+
+export default CatchSunBanner;
