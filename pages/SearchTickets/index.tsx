a4d7ecf977e65b9cd4c083dcbc2417eb133186ae
RobertVai <vaitechovskij@gmail.com>
2025-08-17 19:32:12 +0300

    created flightresults component, ticket list, checkbox

--- pages/SearchTickets/index.tsx
+++ pages/SearchTickets/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Header from '@/components/Header/Header'
 import styles from "./searchtickets.module.css"
+import FlightSearch from "@/components/FlightSearch/FlightSearch";
+import FlightResults from '@/components/FlightResults/FlightResults';
+import Footer from '@/components/Footer/Footer';
 
 
 
 function index() {
   return (
+    <div className={styles.pagewrapper}>
     <div className={styles.container}>
         <Header />
+        <FlightSearch />
+        <FlightResults />
+      
         
+    </div>
+    <Footer />
     </div>
   )
 }
